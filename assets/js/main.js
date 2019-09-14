5e69a0cdaf0b00a069cfaa66bf4f3056e0a0c313
Tengiz Mindiashvili <tengizmindiashvili@gmail.com>
2019-09-14 17:39:00 +0400

    class change, JS update for burger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
-let buttons = document.getElementsByClassName("damateba");
-    
+let add_buttons = document.getElementsByClassName('addtocart');
+console.log(add_buttons);
 
-    for(let i =0;i<buttons.length;i++){
-        buttons[i].addEventListener('click',function(){
-            this.classList.toggle('damatebulia');
-            if(this.classList.contains("damatebulia")){
-                this.textContent = "დამატებულია"
-            }
-            else{
-                this.textContent = "დამატება"
-            }
+for (let i=0; i < add_buttons.length;
+    i++) {
+        add_buttons[i].addEventListener('click', function(){
+        this.classList.toggle('added');
+        if(this.classList.contains('added')){
+            this.textContent = 'დამატებულია';
+        } else {
+            this.textContent = 'დამატება';
+        }
         })
-       
     }
 
-
+    let burger = document.getElementsById('burger');
+    let menu = document.getElementsByClassId('menu')
+    burger.addEventListener('click',function(){
+    this.classList.toggle('active');
+    menu.classList.toggle('open');
+    })
